2545c64bc0ab4f232886e86af5be7ef6d6048cf8
Dalchandsahu <124562532+Dalchandsahu@users.noreply.github.com>
2023-02-08 11:11:23 +0530

    Update SecondDemo.js

--- SecondDemo.js
+++ SecondDemo.js
@@ -6,3 +6,26 @@ items[2].style.backgroundColor = 'Green';
 for(var i =0; i<items.length; i++){
     items[i].style.fontWeight = 'bold';
 }
+// GETELEMENTSBYTAGNAME //
+var li = document.getElementsByTagName('li');
+console.log (li);
+console.log(li[1]);
+li[2].textContent = '3rd elemennt';
+li[2].style.backgroundColor = 'Green'; 
+for(var i =0; i<li.length; i++){
+    li[i].style.fontWeight = 'bold';
+}
+
+// QuerySelector//
+var SecondItems =document.QuerySelector('list-group-item : nth-child(2)');
+SecondItems.style.backgroundColor = 'Green';
+
+// QUERYSELECTORALL //
+var titles = document.querySelectorAll('title');
+console.log(titles);
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i =0; i<odd.length; i++){
+    odd[i].style.backgroundColor = 'green';
+}
